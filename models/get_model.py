5f883898283fdca836d0003283497dca8cddf57f
Ada <the.nostra.tymus@gmail.com>
2022-10-13 19:56:18 -0400

    better notebooks and implemented new CNNs

--- models/get_model.py
+++ models/get_model.py
@@ -1,15 +1,19 @@
-import models.example as example
 import models.lenet as lenet
 import models.mlp as mlp
-import models.resnet as resnet
+import models.cnn1 as cnn1
+import models.cnn2 as cnn2
+import models.cnn1 as cnn3
 
 
 def get_model(name):
-    if name == "example":
-        return example.Model
-    elif name == "lenet":
+    if name == "lenet":
         return lenet.Model
     elif name == "mlp":
         return mlp.Model
-    # elif name == "resnet":
-    #     return resnet.Model
+    elif name == "cnn1":
+        return cnn1.Model
+    elif name == "cnn2":
+        return cnn2.Model
+    elif name == "cnn3":
+        return cnn3.Model
+
--- run.sh
+++ run.sh
@@ -1,22 +1,5 @@
-python -m src.cli --epochs 10 --class-type mlp --name mlp_small_batch --learning-rate 0.001 --batch_size 60
-python -m src.cli --epochs 10 --class-type mlp --name mlp_large_batch --learning-rate 0.001 --batch_size 800
-python -m src.cli --epochs 10 --class-type mlp --name mlp_small_lr --learning-rate 0.0001
-python -m src.cli --epochs 10 --class-type mlp --name mlp_small_lr --learning-rate 0.0001
-python -m src.cli --epochs 10 --class-type mlp --name mlp_large_lr --learning-rate 0.01
-
-python -m src.cli --epochs 10 --class-type example --name example_small_batch --learning-rate 0.001 --batch_size 60
-python -m src.cli --epochs 10 --class-type example --name example_large_batch --learning-rate 0.001 --batch_size 800
-python -m src.cli --epochs 10 --class-type example --name example_small_lr --learning-rate 0.0001
-python -m src.cli --epochs 10 --class-type example --name example_small_lr --learning-rate 0.0001
-python -m src.cli --epochs 10 --class-type example --name example_large_lr --learning-rate 0.01
-
-
-python -m src.cli --epochs 10 --class-type mlp --name mlp_base --learning-rate 0.001
-python -m src.cli --epochs 5 --class-type mlp --name mlp5 --learning-rate 0.001
-python -m src.cli --epochs 20 --class-type mlp --name mlp20 --learning-rate 0.001
-python -m src.cli --epochs 100 --class-type mlp --name mlp100 --learning-rate 0.001
-
-python -m src.cli --epochs 10 --class-type example --name example_base --learning-rate 0.001
-python -m src.cli --epochs 5 --class-type example --name example5 --learning-rate 0.001
-python -m src.cli --epochs 20 --class-type example --name example20 --learning-rate 0.001
-python -m src.cli --epochs 100 --class-type example --name example100 --learning-rate 0.001
+python -m src.cli --epochs 20 --class-type mlp --name mlp_base --learning-rate 0.001
+python -m src.cli --epochs 20 --class-type lenet --name lenet_base --learning-rate 0.001
+python -m src.cli --epochs 20 --class-type cnn1 --name cnn1_base --learning-rate 0.001
+python -m src.cli --epochs 20 --class-type cnn2 --name cnn2_base --learning-rate 0.001
+python -m src.cli --epochs 20 --class-type cnn3 --name cnn3_base --learning-rate 0.001
