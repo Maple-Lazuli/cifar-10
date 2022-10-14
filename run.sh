3d2cc8b4e551472d9c31751695dc511f33cb7ec1
Ada <the.nostra.tymus@gmail.com>
2022-10-13 20:19:18 -0400

    updated the run script and made more jupyter notebooks

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python -m src.cli --epochs 20 --class-type mlp --name mlp_base --learning-rate 0.001
-python -m src.cli --epochs 20 --class-type lenet --name lenet_base --learning-rate 0.001
-python -m src.cli --epochs 20 --class-type cnn1 --name cnn1_base --learning-rate 0.001
-python -m src.cli --epochs 20 --class-type cnn2 --name cnn2_base --learning-rate 0.001
-python -m src.cli --epochs 20 --class-type cnn3 --name cnn3_base --learning-rate 0.001
+python -m src.cli --epochs 40 --class-type mlp --name mlp_base --learning-rate 0.001
+python -m src.cli --epochs 40 --class-type lenet --name lenet_base --learning-rate 0.001
+python -m src.cli --epochs 40 --class-type cnn1 --name cnn1_base --learning-rate 0.001
+python -m src.cli --epochs 40 --class-type cnn2 --name cnn2_base --learning-rate 0.001
+python -m src.cli --epochs 40 --class-type cnn3 --name cnn3_base --learning-rate 0.001
